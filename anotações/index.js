ecf15b919bd038475d96a91e8ec4efc5af1ae7ed
Miqueias <miqueiaslqueiroz@gmail.com>
2023-06-23 21:00:52 -0300

    commit inicial

---
+++ anotações/index.js
@@ -0,0 +1,15 @@
+console.log('Miqueias') //string
+console.log(123) //int
+
+let nomeCliente = 'Miqueias';
+
+var nomeCliente2 = 'Leandro';
+
+// sempre usar o let para Varieaveis
+
+const nomeCliente3 = 'Kaio';
+
+// sempre usar const para constantes
+
+// foi isso o que eu aprendi hoje 22/06/23
+
